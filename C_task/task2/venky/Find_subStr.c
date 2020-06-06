ea83305997adc0d2edeb9494fecbd419c919edab
vsmahi123 <vsmahindrakar97@gmail.com>
2020-06-06 17:33:13 +0530

    logic change added strstr case insensitive functionality

--- C_task/task2/venky/Find_subStr.c
+++ C_task/task2/venky/Find_subStr.c
@@ -3,41 +3,63 @@
 #include<stdlib.h>
 /*My changes Here*/
 #define msg(x) printf("%s : ",x)
+
+char * strstr_case_insensitive(const char *ipStr,const char *sub);
 int main()
 {
-	char ipStr[100] = {'\0',},sub[10];
+	char ipStr[100] ={'\0',},sub[100];
 	char i=0, found =0;
+	char * ptr =NULL;
 	fflush(stdin);
 	msg("enter string");
 	scanf("%[^\n]s",ipStr);
-	
 	msg("enter SUBstring");
 	scanf("%s",sub);
-	while(ipStr[i]!= '\0' && i<101)
-	{
-		if((ipStr[i] == sub[0] || ipStr[i] == sub[0]-32 || ipStr[i] == sub[0]+32) && (ipStr[i+1] == sub[1] || ipStr[i+1] == sub[1]+32 || ipStr[i+1] == sub[1]-32))
-		{
-			if(i== 0 || ipStr[i-1] == ' ')
-			{
-				while(i<101 && ipStr[i] != ' ' /*|| ipStr[i] != '\n' */)
-				{
-					printf("%c",ipStr[i]);
-					i++;
-				}
-				found = 1;
-
-				
-			}
-			printf("\n");
-			
-		}
-   // getchar();
-   i++;
-	}
-    if (0 == found)
+	ptr = strstr_case_insensitive(ipStr,sub);
+    if (NULL == ptr)
     {
        msg("--->sub string not found<---");
     }
+    else
+    {
+	    printf("-------\n%s\n-------\n",ptr);
+    }
 
 	return 0;
+}
+char * strstr_case_insensitive(const char *ipStr,const char *sub)
+{
+	char * point = NULL;
+	int i = 0,j= 0;
+	for(i=0;ipStr[i]!= '\0';i++)
+	{
+		if(((ipStr[i]>=97?ipStr[i]:ipStr[i]+32) ==(sub[j]>=97?sub[j]:sub[j]+32 ) )&& (i==0 || ipStr[i-1]== ' '))
+		{
+              		for(j=0;ipStr[i]!='\0' && sub[j]!='\0';i++,j++)
+	      		{
+				
+		               if((ipStr[i]>=97?ipStr[i]:ipStr[i]+32) !=(sub[j]>=97?sub[j]:sub[j]+32 ) )
+				{
+					break;
+				}
+		      
+	      		}
+
+
+			if(sub[j] == '\0')
+			{
+				point = (char *)ipStr+i-j;
+				break;
+			}
+			else
+			{
+				j=0;
+			}
+			
+		}
+
+               
+	}
+
+	return point;
 }
